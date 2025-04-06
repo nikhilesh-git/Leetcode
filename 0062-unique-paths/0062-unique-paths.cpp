57f267b7331289d617d2c16210498fd60c50fe2e
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-04-06 14:39:31 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         double res=1;
-        for(int i=1;i<=(m-1);++i){
+        int i;
+        for( i=1;i<=(m-1);++i){
             res=res*((m+n-2)-(m-1) + i)/i;
         }
         return (int)res;
