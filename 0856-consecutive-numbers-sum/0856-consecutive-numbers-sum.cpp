51eca6c33b937254b6bb00263a23c4742b2a6b7d
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-06-15 16:44:54 +0530

    Time: 2 ms (49.75%), Space: 7.9 MB (35.25%) - LeetHub

--- 0856-consecutive-numbers-sum/0856-consecutive-numbers-sum.cpp
+++ 0856-consecutive-numbers-sum/0856-consecutive-numbers-sum.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int consecutiveNumbersSum(int n) {
-        int count=0;
-        for(long long k=1;k<sqrt(2*n);++k){
-            if(k*(k-1)/2 < n && (n-(k*(k-1))/2)%k==0){
+        int count = 0;
+        for (int k = 1; k * (k - 1) / 2 < n; ++k) {
+            int numerator = n - k * (k - 1) / 2;
+            if (numerator % k == 0) {
                 count++;
             }
         }
