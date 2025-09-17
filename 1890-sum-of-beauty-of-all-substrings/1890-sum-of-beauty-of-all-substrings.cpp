a25040a94968c5717308a706fe83a115e87c68c6
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-09-17 22:04:25 +0530

    Time: 111 ms (55.1%), Space: 11.1 MB (74.32%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int res=0;
-        vector<int> prefix(26,0);
-        for(int i=0;i<s.size();++i){
-            for(int j=i;j<s.size();++j){
-                for(int k=i;k<=j;++k){
-                    prefix[s[k]-'a']++;
-                }
-                int maxFreq=INT_MIN,minFreq=INT_MAX;
-                for(int l=0;l<26;++l){
-                    if(prefix[l]!=0 &&prefix[l]<minFreq){
-                        minFreq=prefix[l];
+        int res = 0;
+        int n = s.size();
+        
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);  // reset for each new start
+            
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+                
+                int maxFreq = INT_MIN, minFreq = INT_MAX;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        maxFreq = max(maxFreq, freq[k]);
+                        minFreq = min(minFreq, freq[k]);
                     }
-                    if(prefix[l]>maxFreq){
-                        maxFreq=prefix[l];
-                    }
-                    prefix[l]=0;
                 }
-                res+=maxFreq-minFreq;
+                
+                res += (maxFreq - minFreq);
             }
         }
+        
         return res;
     }
 };
