d4e96eefa1eb7104c9c3153956cb82e3520de1b6
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-03 12:32:23 +0530

    Time: 0 ms (100%), Space: 8.2 MB (80.83%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,19 +12,39 @@ public:
     // }
 
     int uniquePaths(int m, int n) {
+
+        //Memoization
         //vector<vector<int>> dp(m,vector<int>(n,-1));
         // return find(m-1,n-1,dp);
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+
+        //Tabulation
+        // vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        // for(int i=1;i<=m;++i){
+        //     for(int j=1;j<=n;++j){
+        //         if(i==1&& j==1){
+        //             dp[i][j]=1;
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        //         }
+        //     }
+        // }
+        // return dp[m][n];
+
+        //Space Optimization
+        vector<int> prev(n+1,0);
+        vector<int> curr(n+1);
         for(int i=1;i<=m;++i){
             for(int j=1;j<=n;++j){
                 if(i==1&& j==1){
-                    dp[i][j]=1;
+                    curr[j]=1;
                 }
                 else{
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    curr[j]=prev[j]+curr[j-1];
                 }
             }
+            prev=curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
