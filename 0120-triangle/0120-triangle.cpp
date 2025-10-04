ab5b0d1e6a4dcd72388f85ee385ff067cd45c807
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-04 10:36:13 +0530

    Time: 0 ms (100%), Space: 12.7 MB (51.87%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<int> prev=triangle[0];
-        int n=triangle.size();
-        for(int i=1;i<n;++i){
-            int k=triangle[i].size();
-            vector<int> curr(k);
-            for(int j=0;j<k;++j){
-                if(j==0){
-                    curr[j]=triangle[i][j]+prev[j];
-                }
-                else if(j==k-1){
-                    curr[j]=triangle[i][j]+prev[j-1];
-                }
-                else{
-                    curr[j]=triangle[i][j]+min(prev[j],prev[j-1]);
-                }
+        int n = triangle.size();
+        vector<int> prev = triangle[0];
+        vector<int> curr; // reuse the same vector
+
+        for (int i = 1; i < n; ++i) {
+            int k = triangle[i].size();
+            curr.resize(k);
+            for (int j = 0; j < k; ++j) {
+                if (j == 0) curr[j] = triangle[i][j] + prev[j];
+                else if (j == k - 1) curr[j] = triangle[i][j] + prev[j - 1];
+                else curr[j] = triangle[i][j] + min(prev[j], prev[j - 1]);
             }
-            prev=curr;
+            prev.swap(curr); // no copy, just pointer swap
         }
-        return *min_element(prev.begin(),prev.end());
+        return *min_element(prev.begin(), prev.end());
+
     }
 };
