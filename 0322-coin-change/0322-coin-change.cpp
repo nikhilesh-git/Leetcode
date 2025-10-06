40a0fe11bd56d256befb194ed156f4d391a4b2fa
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-06 22:25:52 +0530

    Time: 15 ms (92.91%), Space: 17.9 MB (83.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -28,7 +28,7 @@ public:
         // return res;
 
 
-        //tabulation
+        //tabulation using n+1 rows
         // vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX-1));
         // for(int i=0;i<=n;++i){
         //     dp[i][0]=0;// if 
@@ -47,22 +47,61 @@ public:
         // }
         // return dp[n][amount]==INT_MAX-1?-1:dp[n][amount];
 
+        //tabulation using n rows
+        // vector<vector<int>> dp(n, vector<int>(amount + 1, INT_MAX - 1));
+        // // Base case: 0 amount requires 0 coins
+        // for (int i = 0; i < n; ++i)
+        //     dp[i][0] = 0;
+        // // Base case for first coin type
+        // for (int j = 0; j <= amount; ++j) {
+        //     if (j % coins[0] == 0)
+        //         dp[0][j] = j / coins[0];
+        // }
+        // // DP transition
+        // for (int i = 1; i < n; ++i) {
+        //     for (int j = 1; j <= amount; ++j) {
+        //         int notTake = dp[i - 1][j];
+        //         int take = INT_MAX - 1;
+        //         if (coins[i] <= j) {
+        //             take = 1 + dp[i][j - coins[i]];
+        //         }
+        //         dp[i][j] = min(take, notTake);
+        //     }
+        // }
+
+        // int ans = dp[n - 1][amount];
+        // return (ans >= INT_MAX - 1) ? -1 : ans;
+
+
         //space optimization
-        vector<int> curr(amount+1);
-        vector<int> prev(amount+1,INT_MAX-1);
-        prev[0]=0;
-        for(int i=1;i<=n;++i){
-            curr[0]=0;
-            for(int j=1;j<=amount;++j){
-                int notTake=prev[j];
-                int take=INT_MAX-1;
-                if(coins[i-1]<=j){
-                    take=1+curr[j-coins[i-1]];
-                }
-                curr[j]=min(take,notTake);
+        // vector<int> curr(amount+1);
+        // vector<int> prev(amount+1,INT_MAX-1);
+        // prev[0]=0;
+        // for(int i=1;i<=n;++i){
+        //     curr[0]=0;
+        //     for(int j=1;j<=amount;++j){
+        //         int notTake=prev[j];
+        //         int take=INT_MAX-1;
+        //         if(coins[i-1]<=j){
+        //             take=1+curr[j-coins[i-1]];
+        //         }
+        //         curr[j]=min(take,notTake);
+        //     }
+        //     prev.swap(curr);
+        // }
+        // return prev[amount]==INT_MAX-1?-1:prev[amount];
+
+
+        //space optimization using one array
+        vector<int> dp(amount + 1, INT_MAX - 1);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = coins[i]; j <= amount; ++j) {
+                dp[j] = min(dp[j], 1 + dp[j - coins[i]]);
             }
-            prev.swap(curr);
         }
-        return prev[amount]==INT_MAX-1?-1:prev[amount];
+
+        return (dp[amount] == INT_MAX - 1) ? -1 : dp[amount];
     }
 };
