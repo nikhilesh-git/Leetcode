2fa2164dc10c86350081ce20d485781f33eabef2
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-06 21:57:10 +0530

    Time: 57 ms (17.01%), Space: 53.1 MB (13.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    // recurison with memoization
     int func(int n,int amount,vector<int> &coins,vector<vector<int>> &dp){
         
         if(amount==0) return 0;
@@ -16,21 +18,33 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         //dp[i][j]=number of coins required using first i coins to get amount j
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        //vector<vector<int>> dp(n,vector<int>(amount+1,-1));
         
-        int res=func(n,amount,coins,dp);
-        if(res==INT_MAX-1){
-            return -1;
-        }
-        return res;
+        //memoization
+        // int res=func(n,amount,coins,dp);
+        // if(res==INT_MAX-1){
+        //     return -1;
+        // }
+        // return res;
 
-        // for(int i=0;i<amount;++i){
-        //     dp[0][i]=-1;
-        // }
-        // for(int i=1;i<n;++i){
-        //     for(int j=1;j<=n;++j){
-        //         dp[i][j]=min()
-        //     }
-        // }
+
+        //tabulation
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX-1));
+        for(int i=0;i<=n;++i){
+            dp[i][0]=0;// if 
+        }
+
+
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=amount;++j){
+                int notTake=dp[i-1][j];
+                int take=INT_MAX-1;
+                if(coins[i-1]<=j){
+                    take=1+dp[i][j-coins[i-1]];
+                }
+                dp[i][j]=min(take,notTake);
+            }
+        }
+        return dp[n][amount]==INT_MAX-1?-1:dp[n][amount];
     }
 };
