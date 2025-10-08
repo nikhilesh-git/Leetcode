33cc96522e2782cf304a93b1beac0b176948aef8
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-08 19:38:28 +0530

    Time: 69 ms (25.66%), Space: 13.5 MB (93.62%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -42,11 +42,49 @@ public:
         // return func(0, 0, m - 1, n, m, grid, dp);
 
         //tabulation
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m)));
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m)));
+
+        // for(int j1=0;j1<m;++j1){
+        //     for(int j2=0;j2<m;++j2){
+        //         dp[n-1][j1][j2]= j1==j2?grid[n-1][j1]:grid[n-1][j1]+grid[n-1][j2];
+        //     }
+        // }
+
+        // vector<int> dj = {-1, 0, 1};
+        // for(int i=n-2;i>=0;--i){
+        //     for(int j1=0;j1<m;++j1){
+        //         for(int j2=0;j2<m;++j2){
+        //             int maxi=-1e8;
+        //             for (int d1 : dj) {
+        //                 for (int d2 : dj) {
+                            
+        //                         int value=0;
+        //                         if (j1 == j2) value = grid[i][j1];
+        //                         else value = grid[i][j1] + grid[i][j2];
+                                
+        //                         if(isValid(i+1,j1+d1,j2+d2,n,m)){
+        //                             value += dp[i+1][j1+d1][j2+d2]; 
+        //                         }
+        //                         else{
+        //                             value=-1e8;
+        //                         }
+        //                         maxi = max(maxi, value);
+        //                 }
+        //             }
+        //             dp[i][j1][j2]=maxi;
+        //         }
+        //     }
+        // }
+        // return dp[0][0][m-1];
+
+
+        //space optimization
+        vector<vector<int>> front(m,vector<int>(m)),curr(m,vector<int>(m));
+        
 
         for(int j1=0;j1<m;++j1){
             for(int j2=0;j2<m;++j2){
-                dp[n-1][j1][j2]= j1==j2?grid[n-1][j1]:grid[n-1][j1]+grid[n-1][j2];
+                front[j1][j2]= j1==j2?grid[n-1][j1]:grid[n-1][j1]+grid[n-1][j2];
             }
         }
 
@@ -57,13 +95,12 @@ public:
                     int maxi=-1e8;
                     for (int d1 : dj) {
                         for (int d2 : dj) {
-                            
                                 int value=0;
                                 if (j1 == j2) value = grid[i][j1];
                                 else value = grid[i][j1] + grid[i][j2];
                                 
                                 if(isValid(i+1,j1+d1,j2+d2,n,m)){
-                                    value += dp[i+1][j1+d1][j2+d2]; 
+                                    value += front[j1+d1][j2+d2]; 
                                 }
                                 else{
                                     value=-1e8;
@@ -71,10 +108,11 @@ public:
                                 maxi = max(maxi, value);
                         }
                     }
-                    dp[i][j1][j2]=maxi;
+                    curr[j1][j2]=maxi;
                 }
             }
+            front=curr;
         }
-        return dp[0][0][m-1];
+        return front[0][m-1];
     }
 };
