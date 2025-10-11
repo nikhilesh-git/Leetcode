5de78bd47b00ef356646a1d1a83461dc96fec072
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-11 22:51:13 +0530

    Time: 7 ms (96.55%), Space: 9.1 MB (82.02%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -21,24 +21,39 @@ public:
         // return func(n-1,m-1,s,t,dp);
 
         //tabulation
-        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1));
-        dp[0][0]=1;
+        // vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1));
+        // dp[0][0]=1;
+        // for(int i=1;i<=n;++i){
+        //     dp[i][0]=1;
+        // }
+        // for(int j=1;j<=m;++j){
+        //     dp[0][j]=0;
+        // }
+        // for(int i=1;i<=n;++i){
+        //     for(int j=1;j<=m;++j){
+        //         if(s[i-1]==t[j-1]){
+        //             dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
+
+        //space optimization
+        vector<unsigned long long> prev(m+1,0);
+        prev[0]=1;
+        
         for(int i=1;i<=n;++i){
-            dp[i][0]=1;
-        }
-        for(int j=1;j<=m;++j){
-            dp[0][j]=0;
-        }
-        for(int i=1;i<=n;++i){
-            for(int j=1;j<=m;++j){
+            for(int j=m;j>=1;--j){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+                    prev[j]=prev[j]+prev[j-1];
                 }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+
             }
         }
-        return dp[n][m];
+        return prev[m];
+        
     }
 };
