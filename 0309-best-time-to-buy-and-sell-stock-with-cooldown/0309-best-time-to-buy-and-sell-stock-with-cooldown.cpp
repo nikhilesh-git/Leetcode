90dc6af7176b78005999db5be2188cb521c13890
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-13 17:30:59 +0530

    Time: 3 ms (36.11%), Space: 17.1 MB (7.43%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -16,7 +16,33 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(2,-1)));
-        return f(0,1,0,prices,dp);
+        //memoization
+        // vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(2,-1)));
+        // return f(0,1,0,prices,dp);
+
+        //tabulation
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int>(2)));
+        for(int buy=0;buy<=1;++buy){
+            for(int cooldown=0;cooldown<=1;++cooldown){
+                dp[n][buy][cooldown]=0;
+            }
+        }
+
+        for(int i=n-1;i>=0;--i){
+            for(int buy=0;buy<=1;++buy){
+                for(int cooldown=0;cooldown<=1;++cooldown){
+                    if(buy==1 && cooldown==0){
+                        dp[i][buy][cooldown]=max(-prices[i]+dp[i+1][0][0],dp[i+1][1][0]);
+                    }
+                    else if(buy==1 && cooldown==1){
+                        dp[i][buy][cooldown]=dp[i+1][1][0];
+                    }
+                    else{
+                        dp[i][buy][cooldown]=max(prices[i]+dp[i+1][1][1],dp[i+1][0][0]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][0];
     }
 };
