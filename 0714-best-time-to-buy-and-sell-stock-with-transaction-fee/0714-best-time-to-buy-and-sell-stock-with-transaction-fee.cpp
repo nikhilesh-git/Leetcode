c6cbcb2bad1662db852758401836b6be1744d5ad
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-13 21:49:46 +0530

    Time: 3 ms (84.38%), Space: 58.9 MB (88.71%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -12,7 +12,41 @@ public:
     }
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return f(0,1,fee,prices,dp);
+        //memoization
+        // vector<vector<int>> dp(n,vector<int>(2,-1));
+        // return f(0,1,fee,prices,dp);
+
+        //tabulation
+        // vector<vector<int>> dp(n+1,vector<int>(2));
+
+        // for(int buy=0;buy<=1;++buy){
+        //     dp[n][buy]=0;
+        // }
+        // for(int i=n-1;i>=0;--i){
+        //     for(int buy=0;buy<=1;++buy){
+        //         if(buy==1){
+        //             dp[i][buy]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+        //         }
+        //         else{
+        //             dp[i][buy]=max((prices[i]-fee)+dp[i+1][1],dp[i+1][0]);
+        //         }
+        //     }
+        // }
+        // return dp[0][1];
+
+        vector<int> front(2,0),curr(2);
+
+        for(int i=n-1;i>=0;--i){
+            for(int buy=0;buy<=1;++buy){
+                if(buy==1){
+                    curr[buy]=max(-prices[i]+front[0],front[1]);
+                }
+                else{
+                    curr[buy]=max((prices[i]-fee)+front[1],front[0]);
+                }
+            }
+            front.swap(curr);
+        }
+        return front[1];        
     }
 };
