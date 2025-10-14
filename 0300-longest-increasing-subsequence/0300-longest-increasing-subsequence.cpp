3d026b548843fc2656db81693c3b6a49159056dd
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-14 15:58:24 +0530

    Time: 1 ms (83.29%), Space: 14 MB (94.7%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -38,25 +38,24 @@ public:
 
         // return dp[0][0];
         //the above tabulation can be made into space optimization with curr & prev
-        vector<int> front(n + 1, 0),curr(n+1,0);
-        for (int i = n - 1; i >= 0; i--) {
-            for (int prev_ind = i - 1; prev_ind >= -1; prev_ind--) {
+        // vector<int> front(n + 1, 0),curr(n+1,0);
+        // for (int i = n - 1; i >= 0; i--) {
+        //     for (int prev_ind = i - 1; prev_ind >= -1; prev_ind--) {
 
-                int notTake = front[prev_ind + 1];
+        //         int notTake = front[prev_ind + 1];
 
-                int take = 0;
-                if (prev_ind == -1 || nums[i] > nums[prev_ind]) {
-                    take = 1 + front[i + 1];
-                }
+        //         int take = 0;
+        //         if (prev_ind == -1 || nums[i] > nums[prev_ind]) {
+        //             take = 1 + front[i + 1];
+        //         }
 
-                curr[prev_ind + 1] = max(take, notTake);
-            }
-            front.swap(curr);
-        }
-        return front[0];
+        //         curr[prev_ind + 1] = max(take, notTake);
+        //     }
+        //     front.swap(curr);
+        // }
+        // return front[0];
         //another version of dp ,uses only 1 array
         // vector<int> dp(n,1);//dp[i]= LIS ending at index i, initially every element is a LIS so initialized with 1 
-        
         
         // for(int i=1;i<n;++i){
         //     for(int j=0;j<i;++j){//check all prev elements
@@ -66,5 +65,19 @@ public:
         //     }
         // }
         // return *max_element(dp.begin(),dp.end());
+
+        //nlog(n) solution
+
+        vector<int> temp;
+        for(int i=0;i<n;++i){
+            auto it=lower_bound(temp.begin(),temp.end(),nums[i]);
+            if(it==temp.end()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                *it=nums[i];
+            }
+        }
+        return temp.size();
     }
 };
