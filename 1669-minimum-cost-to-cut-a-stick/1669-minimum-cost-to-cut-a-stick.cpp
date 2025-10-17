ad3d8f92f0cefd8ea5422c4a743e70efe4fe2780
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-17 14:17:09 +0530

    Time: 23 ms (64.5%), Space: 14.1 MB (24.8%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -14,7 +14,23 @@ public:
         sort(cuts.begin(),cuts.end());
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
-        vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
-        return f(1,c,cuts,dp);
+        // vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
+        // return f(1,c,cuts,dp);
+        vector<vector<int>> dp(c+2,vector<int>(c+2));
+        for(int i=0;i<=c;++i){
+            for(int j=0;j<i;++j){
+                dp[i][j]=0;
+            }
+        }
+        for(int i=c;i>=1;--i){
+            for(int j=i;j<=c;++j){
+                int mini=1e9;
+                for(int k=i;k<=j;++k){
+                    mini=min(mini,cuts[j+1]-cuts[i-1] + dp[i][k-1]+dp[k+1][j]);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][c];
     }
 };
