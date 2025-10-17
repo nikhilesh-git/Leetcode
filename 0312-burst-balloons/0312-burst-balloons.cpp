7ee46acb9d2fce89c6f7680c1de6b8b4ee347471
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-17 18:08:36 +0530

    Time: 94 ms (77.46%), Space: 14 MB (29.48%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -14,7 +14,22 @@ public:
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return f(1,n,nums,dp);
+        //memoizaiton
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return f(1,n,nums,dp);
+
+        //tabulation
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+
+        for(int i=n;i>=1;--i){
+            for(int j=i;j<=n;++j){
+                int maxi=INT_MIN;
+                for(int ind=i;ind<=j;++ind){
+                    maxi=max(maxi,nums[i-1]*nums[ind]*nums[j+1]+dp[i][ind-1]+dp[ind+1][j]);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+        return dp[1][n];
     }
 };
