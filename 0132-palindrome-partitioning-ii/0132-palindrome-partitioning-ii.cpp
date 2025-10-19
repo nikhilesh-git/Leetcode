9465523ce9e77e8af140385871868f9655d5a057
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-19 15:48:06 +0530

    Time: 35 ms (85.82%), Space: 12 MB (49.06%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -28,16 +28,44 @@ public:
         // return f(0,s,dp)-1;
 
         //tabu
-        vector<int> dp(n+1,0);
-        for(int i=n-1;i>=0;--i){
-            int mini=1e9;
-            for(int j=i;j<s.size();++j){
-                if(isPalindrome(i,j,s)){
-                    mini=min(mini,1+dp[j+1]);
-                }           
+        // vector<int> dp(n+1,0);
+        // for(int i=n-1;i>=0;--i){
+        //     int mini=1e9;
+        //     for(int j=i;j<s.size();++j){
+        //         if(isPalindrome(i,j,s)){
+        //             mini=min(mini,1+dp[j+1]);
+        //         }           
+        //     }
+        //     dp[i]=mini;
+        // }
+        // return dp[0]-1;
+
+        vector<vector<bool>> isPal(n, vector<bool>(n, false));
+        vector<int> dp(n, 0);
+
+        // Step 1: Precompute palindrome table
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (s[i] == s[j] && (j - i <= 2 || isPal[i + 1][j - 1])) {
+                    isPal[i][j] = true;
+                }
             }
-            dp[i]=mini;
         }
-        return dp[0]-1;
+
+        // Step 2: DP to compute min cuts
+        for (int i = 0; i < n; ++i) {
+            if (isPal[0][i]) {
+                dp[i] = 0; // no cut needed if whole prefix is palindrome
+            } else {
+                dp[i] = i; // max cuts possible (cut between every char)
+                for (int j = 0; j < i; ++j) {
+                    if (isPal[j + 1][i]) {
+                        dp[i] = min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
     }
 };
