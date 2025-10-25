7ff774f4d07a061d2fa86da8136ca6b4b34a51fe
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-10-25 13:49:21 +0530

    Time: 0 ms (100%), Space: 10 MB (59.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -12,9 +12,26 @@ public:
         f(i+1,nums,res,subset,n);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        //recursion
+        // vector<vector<int>> res;
+        // vector<int> subset;
+        // f(0,nums,res,subset,nums.size());
+        // return res;
+
+        //power set
         vector<vector<int>> res;
-        vector<int> subset;
-        f(0,nums,res,subset,nums.size());
+
+        int subsets=1<<nums.size();//total possible subsets = 2^n
+        for(int num=0;num<subsets;++num){
+            vector<int> arr;
+            for(int i=0;i<nums.size();++i){
+                if(num & 1<<i){//if ith bit is1 , then add it to res
+                    arr.push_back(nums[i]);
+                }
+            }
+            res.push_back(arr);
+        }
         return res;
     }
+    
 };
