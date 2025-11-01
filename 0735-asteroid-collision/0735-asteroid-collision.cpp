378f72e505c8f68875c968059c5b8fd597493f3a
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-11-01 21:10:28 +0530

    Time: 0 ms (100%), Space: 21.8 MB (73.43%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,34 +3,38 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
         vector<int> res;
-        int i=0;
-        while(i<asteroids.size()){
-            if(asteroids[i]>0){
+        int i = 0;
+
+        while (i < asteroids.size()) {
+            if (asteroids[i] > 0) {
                 st.push(asteroids[i]);
-            }
-            else{
-                while(!st.empty() && st.top()<abs(asteroids[i])){
+            } 
+            else {
+                // Handle collisions
+                while (!st.empty() && st.top() < abs(asteroids[i])) {
                     st.pop();
                 }
 
-                if(!st.empty() && st.top()==abs(asteroids[i])){
-                    st.pop();
+                if (!st.empty() && st.top() == abs(asteroids[i])) {
+                    st.pop(); // both destroy each other
+                } 
+                else if (st.empty()) {
+                    res.push_back(asteroids[i]); // negative survives
                 }
-                else if(st.empty()){
-                    res.push_back(asteroids[i]);
-                }
-
             }
             i++;
         }
-        vector<int> arr;
-        while(!st.empty()){
-            arr.push_back(st.top());
+
+        // Now add surviving positive asteroids from stack to res (in correct order)
+        // Since stack stores them from left to right, we need them reversed
+        // We'll reverse the portion we append, without extra array
+        int count = st.size();
+        res.resize(res.size() + count);
+        for (int j = res.size() - 1; j >= 0 && !st.empty(); --j) {
+            res[j] = st.top();
             st.pop();
         }
-        for(int i=arr.size()-1;i>=0;--i){
-            res.push_back(arr[i]);
-        }
+
         return res;
     }
 };
