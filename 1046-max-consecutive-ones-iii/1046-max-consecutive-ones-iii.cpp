2b812dd57b5d48bd3fb01af6785a902675d0e8a0
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-11-04 12:16:47 +0530

    Time: 0 ms (100%), Space: 69.5 MB (91.09%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -5,18 +5,38 @@ public:
         int i=0,j=0;
         int zeroes=0;
         int maxi=0;
+
+        //(2N) approach
+        // while(i<n){
+        //     if(nums[i]==0){
+        //         zeroes++;
+        //     }
+
+        //     while(zeroes>k){
+        //         if(nums[j]==0){
+        //             zeroes--;
+        //         }
+        //         j++;
+        //     }
+        //     maxi=max(maxi,i-j+1);
+        //     i++;
+        // }
+
+        //O(N)
         while(i<n){
             if(nums[i]==0){
                 zeroes++;
             }
 
-            while(zeroes>k){
+            if(zeroes<=k){
+                maxi=max(maxi,i-j+1);
+            }
+            else{
                 if(nums[j]==0){
                     zeroes--;
                 }
                 j++;
             }
-            maxi=max(maxi,i-j+1);
             i++;
         }
         return maxi;
