b2609f5669f159cab6d35c54a66b592d8c72caf5
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-11-07 19:39:12 +0530

    Time: 47 ms (31.8%), Space: 84.7 MB (10.55%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -5,22 +5,37 @@ public:
         unordered_map<int,int> mpp;
         int start=0,end=0;
         int maxi=0;
+        // while(end<fruits.size()){
+        //     mpp[fruits[end]]++;
+        //     if(mpp.size()>2){
+        //         while(mpp.size()>2){
+        //             mpp[fruits[start]]--;
+        //             if(mpp[fruits[start]]==0){
+        //                 mpp.erase(fruits[start]);
+        //             }
+        //             start++;
+        //         }
+        //     }
+
+        //     maxi=max(maxi,end-start+1);
+        //     end++;            
+        // }
+
         while(end<fruits.size()){
             mpp[fruits[end]]++;
-            if(mpp.size()>2){
-                while(mpp.size()>2){
-                    mpp[fruits[start]]--;
-                    if(mpp[fruits[start]]==0){
-                        mpp.erase(fruits[start]);
-                    }
-                    start++;
-                }
+            if(mpp.size()<=2){
+                maxi=max(maxi,end-start+1);
             }
-
-            maxi=max(maxi,end-start+1);
+            else{
+                mpp[fruits[start]]--;
+                if(mpp[fruits[start]]==0){
+                    mpp.erase(fruits[start]);
+                }
+                start++;
+            }
+            
             end++;            
         }
-
         return maxi;
     }
 };
