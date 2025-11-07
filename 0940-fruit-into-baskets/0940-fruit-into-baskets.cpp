f6fded50d80f61223956a9447f7906af148b77f2
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-11-07 19:15:24 +0530

    Time: 59 ms (6.43%), Space: 85 MB (10.55%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,45 +1,29 @@
-#include <algorithm>
 
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int firstfruit=-1,secondfruit=-1;
+        unordered_map<int,int> mpp;
         int start=0,end=0;
         int fruit1count=0,fruit2count=0;
         int maxi=0;
         while(end<fruits.size()){
-
-            if(fruits[end]==firstfruit ){
-                fruit1count++;
-                end++;
+            if(mpp.size()<2 || mpp.find(fruits[end])!=mpp.end()){
+                mpp[fruits[end]]++;
+                
             }
-            else if( fruits[end]==secondfruit){
-                fruit2count++;
-                end++;
-            }
-            else if(fruit1count==0){
-                firstfruit=fruits[end];
-                fruit1count++;
-                end++;
-            }
-            else if(fruit2count==0){
-                secondfruit=fruits[end];
-                fruit2count++;
-                end++;
-            }
-            else{
-                while(fruit1count!=0 && fruit2count!=0){
-                    if(fruits[start]==firstfruit){
-                        fruit1count--;
-                    }
-                    else{
-                        fruit2count--;
+            else if(mpp.find(fruits[end])==mpp.end()){
+                while(start<fruits.size() && mpp.size()>=2){
+                    mpp[fruits[start]]--;
+                    if(mpp[fruits[start]]==0){
+                        mpp.erase(fruits[start]);
                     }
                     start++;
                 }
+                mpp[fruits[end]]++;
             }
+            maxi=max(maxi,end-start+1);
+            end++;
             
-            maxi=max(maxi,(fruit1count+fruit2count));
         }
 
         return maxi;
