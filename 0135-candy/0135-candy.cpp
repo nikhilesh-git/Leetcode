d33fd23da828c368749e91e49f4aec31b402142c
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2026-01-05 23:04:54 +0530

    Time: 0 ms (100%), Space: 22.2 MB (79.7%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int i=1,count=1;
+        int n=ratings.size();
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                count++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                count+=peak;
+                
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                
+                count+=down;
+                down++;
+                i++;
+            }
+            if(down>peak){
+                count=count+(down-peak);
+            }
+        }      
+        return count;
+    }
+};
