9ffff65902bc35414097a08d2be52a7444226517
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2026-01-05 16:05:21 +0530

    Time: 47 ms (51.35%), Space: 94 MB (36.12%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -7,8 +7,7 @@ public:
         sort(intervals.begin(),intervals.end(),cmp);
         int n=intervals.size();
         int count=0;
-        vector<int> curr=intervals[0];
-        int endingInterval=intervals[0][1];
+        int endingInterval=intervals[0][1];//previous ending interval to compare with current start interval to check for overlap
         for(int i=1;i<n;++i){
             if(intervals[i][0]<endingInterval){
                 count++;
