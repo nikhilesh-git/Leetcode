1796b0e099ad13b12c59ece4f35c471d904c631e
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2026-01-11 16:05:26 +0530

    Time: 0 ms (100%), Space: 14.9 MB (84.57%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,36 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if(root==NULL) return {};
-        map<int,int> mpp;
-        queue<pair<int,TreeNode*>> q;
-        q.push({0,root});
-        while(!q.empty()){
-            int height=q.front().first;
-            TreeNode * node=q.front().second;
-            q.pop();
-            mpp[height]=node->val;
-            if(node->left!=NULL){
-                q.push({height+1,node->left});
-            }
-            if(node->right!=NULL){
-                q.push({height+1,node->right});
-            }
-        }
         vector<int> res;
-        for(auto &it:mpp){
-            res.push_back(it.second);
+        if (root == NULL) return res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                // last node of this level
+                if (i == size - 1) {
+                    res.push_back(node->val);
+                }
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
         }
         return res;
     }
