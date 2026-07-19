c1399df4871d2242f4676e11218e168a84a63eb9
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2026-07-19 22:36:56 +0530

    Time: 0 ms (100%), Space: 20.7 MB (29.04%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        //recursion, memoization, dp
         int n=nums.size();
-        
-        int l=0,r=0;
-        int jumps=0;
-        while(r<n-1){
-            int farthest=0;
-            for(int i=l;i<=r;++i){
-                farthest=max(farthest,i+nums[i]);
+        int farthest=0,jumps=0,currentEnd=0;
+        for(int i=0;i<n-1;++i){
+            farthest=max(farthest,i+nums[i]);
+
+            if(i==currentEnd){
+                jumps++;
+                currentEnd=farthest;
             }
-            if(farthest<=r){
-                return -1;
-            }
-            l=r+1;
-            r=farthest;
-            jumps++;
         }
         return jumps;
     }
